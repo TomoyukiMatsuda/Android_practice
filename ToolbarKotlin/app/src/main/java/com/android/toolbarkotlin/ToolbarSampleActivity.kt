e41bb410c8e485ea05e11af0e838787230fef0a8
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2020-11-07 15:34:02 +0900

    Toolbar Kotlin を学習

--- ToolbarKotlin/app/src/main/java/com/android/toolbarkotlin/ToolbarSampleActivity.kt
+++ ToolbarKotlin/app/src/main/java/com/android/toolbarkotlin/ToolbarSampleActivity.kt
@@ -12,18 +12,17 @@ class ToolbarSampleActivity : AppCompatActivity() {
 
         // activity_toolbar_sample.xml からToolbar要素を取得
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        // ツールバータイトルをセット
-        toolbar.title = "ツールバーサンプル"
-        // アクションバーにtoolbarをセット
+        // アクションバーにツールバーをセット
         setSupportActionBar(toolbar)
         // ツールバーに戻るボタンを設置
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    // ツールバーのアイテムを押した時の処理を記述（今回は戻るボタンのみ）
+    // ツールバーのアイテムを押した時の処理を記述（今回は戻るボタンのみのため、戻るボタンを押した時の処理しか記述していない）
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // android.R.id.home に戻るボタンを押した時の item が入ってくる
+        // android.R.id.home に戻るボタンを押した時のidが取得できる
         if (item.itemId == android.R.id.home) {
+            // 今回はActivityを終了させている
             finish()
         }
         return super.onOptionsItemSelected(item)
