9feddae8e6294ddf9f5741238b5ec28ebd010092
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2020-11-11 10:10:02 +0900

    DataBindingオブジェクトセットの学習

--- DataBindingJava/app/src/main/java/com/android/databindingjava/MainActivity.java
+++ DataBindingJava/app/src/main/java/com/android/databindingjava/MainActivity.java
@@ -1,14 +1,24 @@
 package com.android.databindingjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.android.databindingjava.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private User mUser = new User("ともゆき");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // activity_main.xml に対応したクラスのインスタンスを作成
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        // activity_main.xmlのuserにmUserをセット
+        binding.setUser(mUser);
     }
 }
