a2d4e1796783c30376caab1f66317e3430d876b9
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-05-24 17:13:41 +0900

    Groupie でリスト表示

--- GroupieSampleApp/app/src/main/java/com/android/groupiesampleapp/MainActivity.kt
+++ GroupieSampleApp/app/src/main/java/com/android/groupiesampleapp/MainActivity.kt
@@ -2,13 +2,29 @@ package com.android.groupiesampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.android.groupiesampleapp.databinding.ActivityMainBinding
 import com.android.groupiesampleapp.databinding.ItemGroupieBinding
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.databinding.BindableItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // バインド
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Groupieのアダプターを生成してセット
+        val groupAdapter = GroupAdapter<GroupieViewHolder>()
+        binding.recyclerView.adapter = groupAdapter
+        // リストを作る
+        val items = listOf("寿司", "ステーキ", "鳥の唐揚げ", "アヒージョ", "バームクーヘン", "ティラミス", "カツ丼")
+        items.forEach { item ->
+            // アダプターに Itemインスタンス を add していく
+            groupAdapter.add(GroupieItem(item))
+        }
+
     }
 }
 
