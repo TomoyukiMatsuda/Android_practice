c51c114938775f712c1c74f9388b18a361f7372b
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-05-24 16:58:22 +0900

    リストアイテムのクラスを作成

--- GroupieSampleApp/app/src/main/java/com/android/groupiesampleapp/MainActivity.kt
+++ GroupieSampleApp/app/src/main/java/com/android/groupiesampleapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.android.groupiesampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.groupiesampleapp.databinding.ItemGroupieBinding
+import com.xwray.groupie.databinding.BindableItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+}
+
+// リストアイテムのクラス
+class GroupieItem(private val text: String) : BindableItem<ItemGroupieBinding>() {
+    // レイアウトを返却
+    override fun getLayout(): Int = R.layout.item_groupie
+
+    // レイアウトの要素とバインド
+    override fun bind(viewBinding: ItemGroupieBinding, position: Int) {
+        viewBinding.text = text
     }
 }
