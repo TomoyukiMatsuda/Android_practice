92d33071828d84a6bb30b19ef69ee838c0ddfa26
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-05-24 16:27:06 +0900

    サンプルプロジェクトを整理

--- DataBindingJava/app/src/main/java/com/android/databindingjava/EventHandlers.java
+++ DataBindingJava/app/src/main/java/com/android/databindingjava/EventHandlers.java
@@ -2,7 +2,17 @@ package com.android.databindingjava;
 
 import android.view.View;
 
-public interface EventHandlers {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public abstract class EventHandlers extends BaseObservable {
     // クリックイベントに対応させたいため、引数はView.OnClickListenerのonClickと同じ（View view）にする
-    void onChangeClick(View view);
+    public abstract void onChangeClick(View view);
+
+    @Bindable
+    public abstract boolean isChecked();
+
+    public abstract void setChecked(boolean isEnabled);
+
+    //MVVM設計の見直し兼DataBindingのお勉強じゃ！！Qiita投稿
 }
--- DataBindingJava/app/src/main/java/com/android/databindingjava/MainActivity.java
+++ DataBindingJava/app/src/main/java/com/android/databindingjava/MainActivity.java
@@ -1,18 +1,19 @@
 package com.android.databindingjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BaseObservable;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import com.android.databindingjava.databinding.ActivityMainBinding;
 
 // EventHandlers（インターフェース） を実装
-public class MainActivity extends AppCompatActivity implements EventHandlers {
+public class MainActivity extends AppCompatActivity ,EventHandlers {
 
-    private Character chara = new Character("ドラえもん");
+    private Character chara = new Character("最初");
+    private boolean isChecked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +31,25 @@ public class MainActivity extends AppCompatActivity implements EventHandlers {
     // button_changeのクリックイベント処理
     @Override
     public void onChangeClick(View view) {
+        setChecked(!isChecked);
 
         // charaのnameの文字列によって、セットする文字列を変える
-        if (chara.getName().equals("ドラえもん")) {
-            chara.setName("のびた");
+        if (isChecked) {
+            chara.setName("チェックありだよ○");
         } else {
-            chara.setName("ドラえもん");
+            chara.setName("チェックなしだよ×");
         }
+    }
+
+    @Override
+    public boolean isChecked() {
+        return this.isChecked;
+    }
+
+    @Override
+    public void setChecked(boolean isEnabled) {
+        this.isChecked = isEnabled;
+        notifyPropertyChanged(BR.checked);
+        notifyPropertyChanged(BR.character);
     }
 }
