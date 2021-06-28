a47bcea308a53dafcdfe338b63dfe20cec3bda05
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-06-28 09:53:12 +0900

    Rx Observable Single の動きを確認

--- rxSample/app/src/main/java/com/android/rxsample/MainActivity.kt
+++ rxSample/app/src/main/java/com/android/rxsample/MainActivity.kt
@@ -3,7 +3,9 @@ package com.android.rxsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.kotlin.subscribeBy
+import java.util.Locale.filter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,13 +16,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rxExecute() {
+
+        // 流れてきた値の数だけonNext される
         Observable.just(1, 2, 3, 4, 5)
             .take(3)
             .filter { it % 2 ==1 }
             .subscribeBy(
-                onNext = { println("onNext: $it") },
+                onNext = {
+                    println("onNext: $it") },
                 onError = { println("エラーーーー")},
                 onComplete = { println("正常に終了！！！！") }
+            )
+
+        // 流せる値は１個だけ
+        Single.just(arrayOf(1, 2, 3, 4, 5, 6, 7))
+            .map {
+                it.filter { i ->
+                    i % 2 == 1
+                }
+            }
+            .subscribeBy(
+                onSuccess = {
+                    println("onSuccess: $it") },
+                onError = { println("エラーーーー")},
             )
     }
 }
